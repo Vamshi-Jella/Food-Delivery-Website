8eead2de1b08e967760e2c833003236ba5daa128
Vamshi-Jella <vamshijella21@gmail.com>
2025-01-10 17:46:34 +0530

    Added Vendor Registration function

--- controllers/vendorControllers.js
+++ controllers/vendorControllers.js
@@ -10,8 +10,25 @@ const vendorRegister=async (req,res) => {
             return res.status(400).json("Email already taken");
         }
         const hashedPassword = await bcrypt.hash(password,10); //password ni hash chestunam, 10 - oka algorithm antey 10 times exicute iyi - final ga vachina hashed password ni e variable ki assign chestunam
+        // req.body - body nunchi vastuna input values ni Database lo store cheyaniki - oka instance ni create cheshi dani dwara , records - Database lo store cheyavachu
+        const newVendor = new Vendor({
+            username,
+            email,
+            password:hashedPassword
+        }); // ela instance ki values pass cheshi store chestam , password ki hashedPassword value ni pass chestam
+        await newVendor.save(); // save method to Database lo save chestam
+        res.status(201).json({message:"Vendor registered successfully"});
+        // 200-299 unna ani kuda success oka codes
+        // if e data successfully ga save ithey ne ah message vastadi
+        // data save ithey - 201  success code to e message response eyamamni meaning above code
+        console.log('registered'); // console lo chupiyaniki
 
     } catch (error) {
-        
+        console.error(error); // asal error enti anedi console lo chupiyaniki
+        res.status(500).json({error:"Internal server error"});
+        // 500 anedi error code
+        // mana response error(status(500)) ithey e message vastadi
     }
 } 
+
+module.exports = {vendorRegister}
